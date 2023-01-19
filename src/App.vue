c811c2bc683197cad1de98b1ea18a0f5756bc5e5
Giuseppe-Dattilo <giuseppe.dattilo19@gmail.com>
2023-01-19 18:10:20 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/Apphero.vue';
+export default {
+  name: "App",
+  data() {
+  },
+
+  components: {
+    AppTitle,
+    AppHero
+  },
+}
+</script>
+
+<template>
+  <app-title></app-title>
+  <app-hero></app-hero>
+</template>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'AppHero',
+    data() {
+        return {
+            image: 'Vite+Vue',
+        }
+    }
+}
+</script>
+
+<template>
+    <img src="../assets/Vite+Vue.png" alt="">
+</template>
